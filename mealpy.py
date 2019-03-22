0ae27685a155ea6a1a3a0f2b9ea52616c5d63133
John Nguyen <ipwnponies@gmail.com>
2019-03-21 20:28:07 -0700

    Appease pylint

--- mealpy.py
+++ mealpy.py
@@ -1,8 +1,8 @@
 import getpass
 import json
-import requests
 import time
 
+import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 BASE_DOMAIN = 'secure.mealpal.com'
@@ -23,104 +23,99 @@ HEADERS = {
 }
 
 
-class MealPal(object):
+class MealPal():
 
     def __init__(self):
-        self.headers = HEADERS
         self.cookies = None
         self.cities = None
         self.schedules = None
 
     def login(self, username, password):
         data = {'username': username, 'password': password}
-        r = requests.post(
-            LOGIN_URL, data=json.dumps(data), headers=self.headers)
-        self.cookies = r.cookies
+        request = requests.post(LOGIN_URL, data=json.dumps(data), headers=HEADERS)
+        self.cookies = request.cookies
         self.cookies.set(LOGGED_IN_COOKIE, 'true', domain=BASE_URL)
-        return r.status_code
+        return request.status_code
 
-    def get_cities(self):
-        if not self.cities:
-            r = requests.post(CITIES_URL, headers=self.headers)
-            self.cities = r.json()['result']
-        return self.cities
+    @staticmethod
+    def get_cities():
+        request = requests.post(CITIES_URL, headers=HEADERS)
+        return request.json()['result']
 
     def get_city(self, city_name):
-        if not self.cities:
-            self.get_cities()
-        city = next((i for i in self.cities if i['name'] == city_name), None)
+        city = next((i for i in self.get_cities() if i['name'] == city_name), None)
         return city
 
     def get_schedules(self, city_name, city_id=None):
         if not city_id:
             city_id = self.get_city(city_name)['objectId']
-        r = requests.get(
-            MENU_URL % city_id, headers=self.headers, cookies=self.cookies)
-        self.schedules = r.json()['schedules']
-        return self.schedules
+        request = requests.get(MENU_URL % city_id, headers=HEADERS, cookies=self.cookies)
+        return request.json()['schedules']
 
-    def get_schedule_by_restaurant_name(
-            self, restaurant_name, city_name=None, city_id=None):
-        if not self.schedules:
-            self.get_schedules(city_name, city_id)
-        restaurant = next(i for i in self.schedules if i['restaurant']['name'] == restaurant_name)
+    def get_schedule_by_restaurant_name(self, restaurant_name, city_name=None, city_id=None):
+        restaurant = next(
+            i
+            for i in self.get_schedules(city_name, city_id)
+            if i['restaurant']['name'] == restaurant_name
+        )
         return restaurant
 
-    def get_schedule_by_meal_name(
-            self, meal_name, city_name=None, city_id=None):
+    def get_schedule_by_meal_name(self, meal_name, city_name=None, city_id=None):
         if not self.schedules:
             self.get_schedules(city_name, city_id)
 
-        return next(i for i in self.schedules if i['meal']['name'] == meal_name)
+        return next(i for i in self.get_schedules(city_name, city_id) if i['meal']['name'] == meal_name)
 
     def reserve_meal(
-            self, timing, restaurant_name=None, meal_name=None, city_name=None,
-            city_id=None, cancel_current_meal=False):
+            self,
+            timing,
+            restaurant_name=None,
+            meal_name=None,
+            city_name=None,
+            city_id=None,
+            cancel_current_meal=False,
+    ):  # pylint: disable=too-many-arguments
         assert restaurant_name or meal_name
         if cancel_current_meal:
             self.cancel_current_meal()
 
         if meal_name:
-            schedule_id = self.get_schedule_by_meal_name(
-                meal_name, city_name, city_id)['id']
+            schedule_id = self.get_schedule_by_meal_name(meal_name, city_name, city_id)['id']
         else:
-            schedule_id = self.get_schedule_by_restaurant_name(
-                restaurant_name, city_name, city_id)['id']
+            schedule_id = self.get_schedule_by_restaurant_name(restaurant_name, city_name, city_id)['id']
 
         reserve_data = {
             'quantity': 1,
             'schedule_id': schedule_id,
             'pickup_time': timing,
-            'source': 'Web'
+            'source': 'Web',
         }
 
-        r = requests.post(
-            RESERVATION_URL, data=json.dumps(reserve_data),
-            headers=self.headers, cookies=self.cookies)
-        return r.status_code
+        request = requests.post(RESERVATION_URL, data=json.dumps(reserve_data), headers=HEADERS, cookies=self.cookies)
+        return request.status_code
 
     def get_current_meal(self):
-        r = requests.post(
-            KITCHEN_URL, headers=self.headers, cookies=self.cookies)
-        return r.json()
+        request = requests.post(KITCHEN_URL, headers=HEADERS, cookies=self.cookies)
+        return request.json()
 
     def cancel_current_meal(self):
-        pass
-
-scheduler = BlockingScheduler()
-print("Enter email: ")
-email = input()
-print("Enter password: ")
-password = getpass.getpass()
+        raise NotImplementedError()
 
 
-@scheduler.scheduled_job('cron', hour=16, minute=59, second=58)
+SCHEDULER = BlockingScheduler()
+print('Enter email: ')
+EMAIL = input()
+print('Enter password: ')
+PASSWORD = getpass.getpass()
+
+
+@SCHEDULER.scheduled_job('cron', hour=16, minute=59, second=58)
 def execute_reserve_meal():
-    mp = MealPal()
+    mealpal = MealPal()
 
     # Try to login
     while True:
-        status_code = mp.login(email, password)
+        status_code = mealpal.login(EMAIL, PASSWORD)
         if status_code == 200:
             print('Logged In!')
             break
@@ -130,21 +125,22 @@ def execute_reserve_meal():
     # Once logged in, try to reserve meal
     while True:
         try:
-            status_code = mp.reserve_meal(
+            status_code = mealpal.reserve_meal(
                 '12:15pm-12:30pm',
                 restaurant_name='Coast Poke Counter - Battery St.',
-                city_name='San Francisco')
+                city_name='San Francisco',
+            )
             if status_code == 200:
                 print('Reservation success!')
                 print('Leave this script running to reschedule again the next day!')
-                return
+                break
             else:
                 print('Reservation error, retrying!')
         except IndexError:
-            print("Retrying...")
+            print('Retrying...')
             time.sleep(0.05)
 
-# scheduler.start()
+# SCHEDULER.start()
 
 
 if __name__ == '__main__':
