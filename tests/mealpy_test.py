b4f3ef5cfa70d75e83f464d3ddf6ed3eecb321bd
John Nguyen <ipwnponies@gmail.com>
2019-04-20 12:30:16 -0700

    Move get_city tests into test class

--- tests/mealpy_test.py
+++ tests/mealpy_test.py
@@ -11,104 +11,107 @@ def mock_responses():
         yield _responses
 
 
-def test_get_city(mock_responses):
-    response = {
-        'result': [
-            {
-                'id': 'mock_id1',
-                'objectId': 'mock_objectId1',
-                'state': 'CA',
-                'name': 'San Francisco',
-                'city_code': 'SFO',
-                'latitude': 'mock_latitude',
-                'longitude': 'mock_longitude',
-                'timezone': -7,
-                'countryCode': 'usa',
-                'countryCodeAlphaTwo': 'us',
-                'defaultLocale': 'en-US',
-                'dinner': False,
-                'neighborhoods': [
-                    {
-                        'id': 'mock_fidi_id',
-                        'name': 'Financial District',
-                    },
-                    {
-                        'id': 'mock_soma_id',
-                        'name': 'SoMa',
-                    },
-                ],
-            },
-            {
-                'id': 'mock_id2',
-                'objectId': 'mock_objectId2',
-                'state': 'WA',
-                'name': 'Seattle',
-                'city_code': 'SEA',
-                'latitude': 'mock_latitude',
-                'longitude': 'mock_longitude',
-                'timezone': -7,
-                'countryCode': 'usa',
-                'countryCodeAlphaTwo': 'us',
-                'defaultLocale': 'en-US',
-                'dinner': False,
-                'neighborhoods': [
-                    {
-                        'id': 'mock_belltown_id',
-                        'name': 'Belltown',
-                    },
-                ],
-            },
-        ],
-    }
+class TestGetCity:
 
-    mock_responses.add(
-        responses.RequestsMock.POST,
-        mealpy.CITIES_URL,
-        json=response,
-    )
+    @staticmethod
+    def test_get_city(mock_responses):
+        response = {
+            'result': [
+                {
+                    'id': 'mock_id1',
+                    'objectId': 'mock_objectId1',
+                    'state': 'CA',
+                    'name': 'San Francisco',
+                    'city_code': 'SFO',
+                    'latitude': 'mock_latitude',
+                    'longitude': 'mock_longitude',
+                    'timezone': -7,
+                    'countryCode': 'usa',
+                    'countryCodeAlphaTwo': 'us',
+                    'defaultLocale': 'en-US',
+                    'dinner': False,
+                    'neighborhoods': [
+                        {
+                            'id': 'mock_fidi_id',
+                            'name': 'Financial District',
+                        },
+                        {
+                            'id': 'mock_soma_id',
+                            'name': 'SoMa',
+                        },
+                    ],
+                },
+                {
+                    'id': 'mock_id2',
+                    'objectId': 'mock_objectId2',
+                    'state': 'WA',
+                    'name': 'Seattle',
+                    'city_code': 'SEA',
+                    'latitude': 'mock_latitude',
+                    'longitude': 'mock_longitude',
+                    'timezone': -7,
+                    'countryCode': 'usa',
+                    'countryCodeAlphaTwo': 'us',
+                    'defaultLocale': 'en-US',
+                    'dinner': False,
+                    'neighborhoods': [
+                        {
+                            'id': 'mock_belltown_id',
+                            'name': 'Belltown',
+                        },
+                    ],
+                },
+            ],
+        }
 
-    mealpal = mealpy.MealPal()
-    city = mealpal.get_city('San Francisco')
+        mock_responses.add(
+            responses.RequestsMock.POST,
+            mealpy.CITIES_URL,
+            json=response,
+        )
 
-    assert city.items() >= {
-        'id': 'mock_id1',
-        'state': 'CA',
-        'name': 'San Francisco',
-    }.items()
+        mealpal = mealpy.MealPal()
+        city = mealpal.get_city('San Francisco')
 
+        assert city.items() >= {
+            'id': 'mock_id1',
+            'state': 'CA',
+            'name': 'San Francisco',
+        }.items()
 
-def test_get_city_not_found(mock_responses):
-    response = {
-        'result': [
-            {
-                'id': 'mock_id1',
-                'objectId': 'mock_objectId1',
-                'state': 'CA',
-                'name': 'San Francisco',
-                'city_code': 'SFO',
-            },
-        ],
-    }
+    @staticmethod
+    def test_get_city_not_found(mock_responses):
+        response = {
+            'result': [
+                {
+                    'id': 'mock_id1',
+                    'objectId': 'mock_objectId1',
+                    'state': 'CA',
+                    'name': 'San Francisco',
+                    'city_code': 'SFO',
+                },
+            ],
+        }
 
-    mock_responses.add(
-        responses.RequestsMock.POST,
-        mealpy.CITIES_URL,
-        json=response,
-    )
+        mock_responses.add(
+            responses.RequestsMock.POST,
+            mealpy.CITIES_URL,
+            json=response,
+        )
 
-    mealpal = mealpy.MealPal()
-    city = mealpal.get_city('Not San Francisco')
+        mealpal = mealpy.MealPal()
+        city = mealpal.get_city('Not San Francisco')
 
-    assert not city
+        assert not city
 
+    @staticmethod
+    def test_get_city_bad_response(mock_responses):
+        mock_responses.add(
+            responses.RequestsMock.POST,
+            mealpy.CITIES_URL,
+            status=400,
+        )
 
-def test_get_city_bad_response(mock_responses):
-    mock_responses.add(
-        responses.RequestsMock.POST,
-        mealpy.CITIES_URL,
-        status=400,
-    )
-
-    mealpal = mealpy.MealPal()
-    with pytest.raises(requests.exceptions.HTTPError):
-        mealpal.get_city('Not San Francisco')
+        mealpal = mealpy.MealPal()
+        with pytest.raises(requests.exceptions.HTTPError):
+            mealpal.get_city('Not San Francisco')
